a6f2a1e2fcee4b6990a080d4398b81f573cac944
rajan-021 <20.rajanmaurya@gmail.com>
2024-10-30 11:27:14 +0530

    cors  file added

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const jsonServer = require('json-server');
+const cors = require('cors');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
+// Enable CORS
 server.use(middlewares);
 server.use(router);
+
+
+const corsOptions = {
+    origin: 'http://localhost:3000', // Replace with your frontend domain
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Define the HTTP methods you want to allow
+    allowedHeaders: ['Content-Type', 'Authorization'], // Define the allowed headers
+  };
+  
+  server.use(cors(corsOptions));
+  
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
